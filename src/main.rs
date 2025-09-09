c8313b2a1f1771e9a58790917f92d2b819520438
Mikołaj Szczęsny <miszczes0@gmail.com>
2025-09-09 21:45:51 +0200

    rrep: baseline functionality working
    next writing error messages to standard Error instead
    of standard Output
    
    Signed-off-by: Mikołaj Szczęsny <miszczes0@gmail.com>

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crust::search;
+use crust::{search, search_case_insensitive};
 use std::env;
 use std::error::Error;
 use std::fs;
@@ -7,6 +7,7 @@ use std::process;
 struct Config {
     query: String,
     file_path: String,
+    ignore_case: bool,
 }
 
 impl Config {
@@ -17,13 +18,25 @@ impl Config {
         let query = args[1].clone();
         let file_path = args[2].clone();
 
-        Ok(Config { query, file_path })
+        let ignore_case = env::var("IGNORE_CASE").is_ok();
+
+        Ok(Config {
+            query,
+            file_path,
+            ignore_case,
+        })
     }
 }
 
 fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.file_path)?;
-    for line in search(&config.query, &contents) {
+    let results = if config.ignore_case {
+        search_case_insensitive(&config.query, &contents)
+    } else {
+        search(&config.query, &contents)
+    };
+
+    for line in results {
         println!("{line}");
     }
 
